38a8db0190ec9f861ae2805a06d7946a7c1603c6
Yifeng Jiang <uprushworld@gmail.com>
2020-05-04 14:59:03 +0800

    add pyspark docker image

---
+++ docker/build-pyspark.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+SPARK_BUILD_VER=2.4.5
+REPO_NAME=uprush
+
+# Build docker image.
+docker build -t apache-spark-pyspark-$SPARK_BUILD_VER -f ./pyspark.Dockerfile .
+
+# Push to docker repository.
+docker tag apache-spark-pyspark-$SPARK_BUILD_VER $REPO_NAME/apache-spark-pyspark:$SPARK_BUILD_VER
+docker push $REPO_NAME/apache-spark-pyspark:$SPARK_BUILD_VER
